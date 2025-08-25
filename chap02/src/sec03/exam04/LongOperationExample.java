2a72d6fbe5ffad4045695b4b2f933deba1088d3e
wjs_813 <jaeseongwi48@gmail.com>
2025-08-25 16:29:59 +0900

    8월25일 수업 연습 내용

---
+++ chap02/src/sec03/exam04/LongOperationExample.java
@@ -0,0 +1,13 @@
+package sec03.exam04;
+
+public class LongOperationExample {
+
+	public static void main(String[] args) {
+		byte value =10;
+		int value2 = 100;
+		long value3 = 1000L;
+		long result = value + value2 + value3;
+		System.out.println(result);
+	}
+
+}
